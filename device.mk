6a10cb3c81e10319ed9d3d7324cf55b8a8cf1769
Joey <joey@evolution-x.org>
2026-03-06 17:17:58 +0900

    raphael: Drop ApertureOverlayDevice

--- device.mk
+++ device.mk
@@ -60,7 +60,6 @@ $(call soong_config_set,xiaomi_msmnile,variant_lib,//$(LOCAL_PATH):libvariant_xi
 
 # Overlays
 PRODUCT_PACKAGES += \
-    ApertureOverlayDevice \
     FaceUnlockServiceOverlayDevice \
     FrameworkResOverlayDevice \
     LineageSDKOverlayDevice \
